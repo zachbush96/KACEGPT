04f7b9f348dcea961d808171c069c981f0d20fe7
zachbush96 <81973485+zachbush96@users.noreply.github.com>
2023-05-25 16:23:44 -0500

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,8 +1,6 @@
-//Append the content of the chrome local storage to the popup.html
+// Get the stored response and append it to the popup.html
 chrome.storage.local.get(["response"], (result) => {
-  chrome.storage.local.get(["response"], (result) => {
-    let response = result.response;
-    let previousResponse = document.getElementById("response").innerHTML;
-    document.getElementById("response").innerHTML = previousResponse + response;
-    });
+  let response = result.response;
+  let previousResponse = document.getElementById("response").innerHTML;
+  document.getElementById("response").innerHTML = previousResponse + response;
 });
